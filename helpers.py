2c125eeb62f2eb28ca2e5c5515b22f3b7fd8ab1e
QuameOphory <quameophory@yahoo.com>
2022-08-17 15:14:10 -0700

    helpers module updated

--- helpers.py
+++ helpers.py
@@ -1,4 +1,7 @@
 def generateID(prefix, qs, length=9):
+    '''
+    This function helps to generate custom IDs for DB models
+    '''
     if qs.count() == 0:
         id = prefix + str(1).zfill(length)
     else:
