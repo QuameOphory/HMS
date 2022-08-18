0d1f9bed879a5dcbb282f98e96590693d4e1b56d
QuameOphory <quameophory@yahoo.com>
2022-08-18 13:09:19 -0700

    check patient_add form

--- records/views.py
+++ records/views.py
@@ -12,13 +12,6 @@ class PatientCreateView(CreateView):
     template_name = "records/patient_add.html"
 
     def form_valid(self, form):
-        # birth_date = form.cleaned_data['BirthDate']
-        # age = helpers.calculateAge(birth_date)
-        # print('-------------------------', age)
-        # if age < 18:
-        #     form.cleaned_data['PatientTypeID'] = CHILD
-        # else:
-        #     form.cleaned_data['PatientTypeID'] = ADULT
 
         self.object = form.save()
         messages.add_message(
