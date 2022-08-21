2a415df5bc5f5c809f1c8df2e2d1036ec75cd4ae
QuameOphory <quameophory@yahoo.com>
2022-08-21 19:32:18 +0000

    setup visitation models --- uncomplete

--- facility/models.py
+++ facility/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+class Branch(models.Model):
+
+    BranchID = models.CharField(_("Branch ID"), max_length=250)
+    BranchName = models.CharField(_("Branch Name"), max_length=250)
+    Description = models.TextField(_("Description"), blank=True, null=True, editable=False)
+    CreatedAt = models.DateField(_("Created At"), auto_now_add=False)
+    Status = models.BooleanField(_("Status"), default=True, editable=False)
+
+    class Meta:
+        verbose_name = _("Branch")
+        verbose_name_plural = _("Branchs")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Branch_detail", kwargs={"pk": self.pk})
+
--- helpers.py
+++ helpers.py
@@ -19,3 +19,24 @@ def calculateAge(birthdate):
     today = date.today()
     age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
     return age
+
+def generateVisitationID(qs, prefix='V'):
+    today = date.today()
+    year = str(today.year)[-2:] #last 2 digits of current year
+    month = str(today.month).zfill(2) # if month is single character, prefix with 0
+    day = str(today.day).zfill(2) # if day is single character, prefix with 0
+    prefix = year + month + day
+    if qs.count() == 0:
+        id_post_fix = '001'
+        visitationID = prefix + id_post_fix
+    else:
+        last_visit_id = qs.last().VisitationID
+        if last_visit_id[0:6] != prefix:
+            id_post_fix = '001'
+            visitationID = prefix + id_post_fix
+        else:
+            postfix = last_visit_id[-3:]
+            increase_id = int(postfix) + 1
+            id_post_fix = str(increase_id)
+            visitationID = prefix + id_post_fix
+    return visitationID
